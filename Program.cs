62def62d572356781b9f5833e3297d64d706a977
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-03-01 05:52:51 +0530

    [Ajay]-Validate phone number-

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@
             regex.ValidateFirstName("Ajay");
             regex.ValidateLastName("Rathod");
             regex.ValidateEmail("abc.xyz@bl.co.in");
+            regex.ValidatePhoneNumber("91 9896271453");
             Console.ReadLine();
         }
     }
